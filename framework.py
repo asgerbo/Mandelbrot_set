af426cee2783fb3a901fab6f44632e3fb72b7316
Asger Bo Rønsholdt <asgerbor@gmail.com>
2023-10-19 03:21:20 +0200

    update 19/10 3.21

--- framework.py
+++ framework.py
@@ -2,83 +2,81 @@ import os
 import time
 from tkinter import *
 from PIL import Image, ImageTk
-from mandelbrot import FractalRenderer  # Assuming the refactored Mandelbrot class is saved in a file named fractal_renderer.py
+from mandelbrot import Mandelbrot_Fractal
 from multiprocessing import freeze_support
 
 
-class FractalViewer(Frame):
-    def __init__(self, parent, canvas_size, x_center=-0.75, y_center=0, scale=1.5, max_iter=None, img_width=None, img_height=None, save_image=False, use_multiprocessing=True):
+class Mandelbrot_Viewer(Frame):
+    """
+    This is the main class for the Mandelbrot Viewer. I use the __init__ statement to initialize the class. The super function states that this is a child class which comes from the frame class. This allows me to use tkinter methods and options that are common to all tkinter widgets. This one is a bit different, since we have set values for the x_center, y_center, scale, max_iter, img_width, img_height and use_multiprocessing. Using x_center = -0.75, y_center = 0 and scale = 2, seems to be good starting points after having played around with the values. A scale too high, and the set will be too small, and a scale too low, and the set will appear too big. 
+    """
+    def __init__(self, parent, canvas_size, x_center = -0.5, y_center = 0, scale = 2, max_iter = None, img_width = None, img_height = None,  use_multiprocessing=True):
         super().__init__(parent)
         self.parent = parent
         self.parent.title("Fractal Viewer")
-        self.pack(fill=BOTH, expand=1)
+        self.pack(fill = BOTH, expand=1)
         self.canvas = Canvas(self)
 
         if img_width is None or img_height is None:
             img_width, img_height = canvas_size, canvas_size
 
-        aspect_ratio = img_width / img_height
+        aspect_ratio = 16 / 9
         self.canvas_width, self.canvas_height = (canvas_size, round(canvas_size / aspect_ratio)) if img_width > img_height else (round(canvas_size * aspect_ratio), canvas_size)
 
-        self.fractal = FractalRenderer(self.canvas_width, self.canvas_height, x_center=x_center, y_center=y_center, scale=scale, max_iter=max_iter, img_width=img_width, img_height=img_height, use_multiprocessing=use_multiprocessing)
+        self.fractal = Mandelbrot_Fractal(self.canvas_width, self.canvas_height, x_center=x_center, y_center=y_center, scale=scale, max_iter=max_iter, img_width=img_width, img_height=img_height, use_multiprocessing=use_multiprocessing)
         self.palette = [(0, 0, 0), (255, 255, 255)]
         self.pixel_colors = []
         self.image = None
-        self.save_image = save_image
-        self.render()
-
-        parent.bind("<Button-1>", self.zoom_in)
-        parent.bind("<Button-3>", self.zoom_out)
-        parent.bind("<Control-1>", self.pan_view)
-
-    def zoom_in(self, event):
-        self.fractal.zoom(event, 'in')
-        self.render()
-
-    def zoom_out(self, event):
-        self.fractal.zoom(event, 'out')
-        self.render()
-
-    def pan_view(self, event):
-        self.fractal.pan(event)
         self.render()
 
     def render(self):
+        """
+        This is the function that renders the image on the canvas. I have used the time package to measure the time it takes to render the image, since I have been testing different computational methods to improve computational speed. Next i call the generate_pixels function from the Mandelbrot_Fractal class. I then assign the colors to the pixels, and finally paint the pixels on the canvas. To initialize the pixels on the canvas, i set the values 0,0 because i want it to fill the canvas starting from the very first pixel in the top left corer (North West). 
+        """
         print('-' * 20)
         start_time = time.time()
         self.fractal.generate_pixels()
         self.assign_colors()
         self.paint_pixels()
-        self.canvas.create_image(0, 0, image=self.background, anchor=NW)
-        self.canvas.pack(fill=BOTH, expand=1)
-        print(f"Rendering took {round(time.time() - start_time, 2)} seconds")
+        self.canvas.create_image(0, 0, image=self.background, anchor = 'nw')
+        self.canvas.pack(fill = 'both', expand = 1)
+        print(f"Generating canvas took {time.time() - start_time:.2f} seconds")
 
     def assign_colors(self):
-        self.pixel_colors = [self.palette[0] if pixel[2] == 0 else self.palette[1] for pixel in self.fractal.pixel_data]
+        """
+        Create an empty list for the pixel colors. I then loop through the pixel data and assign the colors to the pixels. I use the palette to assign the colors. The palette is a list of tuples, where each tuple contains the values for the color. The first tuple is the color for the pixels that are in the mandelbrot set, and the second tuple is the color for the pixels that are not in the mandelbrot set.
+        """
+        self.pixel_colors = []
+        for pixel in self.fractal.pixel_data:
+            x, y, iteration_count = pixel
+            if iteration_count == 0:
+                color = self.palette[0]
+            else:
+                color = self.palette[1]
+            self.pixel_colors.append(color)
 
     def paint_pixels(self):
+        """
+        I use the Image package to create a new image. I then use the load function to load the image into memory. I then loop through the pixel data and assign the colors to the pixels. I then use the ImageTk package to convert the image to a tkinter image. I then resize the image to fit the canvas.
+        """
         img = Image.new('RGB', (self.fractal.img_width, self.fractal.img_height), "black")
         pixel_map = img.load()
         for idx, pixel in enumerate(self.fractal.pixel_data):
             pixel_map[int(pixel[0]), int(pixel[1])] = self.pixel_colors[idx]
-        self.image = img
-        if self.save_image:
-            self.save_image_to_disk()
         photo_img = ImageTk.PhotoImage(img.resize((self.canvas_width, self.canvas_height)))
         self.background = photo_img
 
-    def save_image_to_disk(self):
-        self.image.save(f"output/{time.strftime('%Y-%m-%d-%H:%M:%S')}.png", "PNG", optimize=True)
-
-
-def main():
+def run_program():
+    """
+    This is the main function. I use the freeze_support function from the multiprocessing package to prevent the program from crashing when running the program on Windows. I then initialize the root window and set the size of the window. I then initialize the Mandelbrot_Viewer class and set the size of the window. I then call the mainloop function to run the program.
+    """
     root = Tk()
     screen_height = round(root.winfo_screenheight() * 0.9)
-    app = FractalViewer(root, screen_height)
+    app = Mandelbrot_Viewer(root, screen_height)
     root.geometry(f"{app.canvas_width}x{app.canvas_height}")
     root.mainloop()
 
 
 if __name__ == '__main__':
     freeze_support()
-    main()
+    run_program()
--- mandelbrot.py
+++ mandelbrot.py
@@ -1,35 +1,34 @@
 from multiprocessing import Pool
 import numpy as np
 
-class FractalRenderer:
-    def __init__(self, canvas_width, canvas_height, x_center=None, y_center=None, scale=None, max_iter=None, img_width=None, img_height=None, zoom=0.1, use_multiprocessing=True):
+class Mandelbrot_Fractal:
+    """
+    I initialize the class with the __init__ statement. I set the default values for all the parameters I use in the class. Furthermore i set the aspect ratio to 16:9 by multipying both the width and height of the canvas created in the framework.py file. 
+    """
+    def __init__(self, canvas_width, canvas_height, x_center = None, y_center = None, scale=None, max_iter = 1000, img_width = None, img_height = None, use_multiprocessing = True):
         self.img_width, self.img_height = (round(canvas_width * 16 / 9), round(canvas_height * 16 / 9)) if img_width is None or img_height is None else (img_width, img_height)
         self.max_iterations = max_iter if max_iter else 500
         self.x_center, self.y_center = x_center, y_center
         self.aspect_ratio = canvas_width / canvas_height
         self.scale = scale
-        self.zoom_level = zoom
         self.use_multiprocessing = use_multiprocessing
         self.update_bounds()
 
     def update_bounds(self):
+        """
+        These are the max and min values for the canvas. I use the scale and aspect ratio to calculate the max and min values for the x and y axis. 
+        """
         self.x_max = self.x_center + self.scale
         self.x_min = self.x_center - self.scale
         self.y_max = self.y_center + self.scale / self.aspect_ratio
         self.y_min = self.y_center - self.scale / self.aspect_ratio
 
-    def zoom(self, event, direction):
-        self.x_center = self.map_value(event.x, 0, self.img_width, self.x_min, self.x_max)
-        self.y_center = self.map_value(event.y, self.img_height, 0, self.y_min, self.y_max)
-        self.scale *= (self.zoom_level if direction == 'in' else 1 / self.zoom_level)
-        self.update_bounds()
-
-    def pan(self, event):
-        self.x_center = convert(event.x, 0, self.img_width, self.x_min, self.x_max)
-        self.y_center = convert(event.y, self.img_height, 0, self.y_min, self.y_max)
-        self.update_bounds()
-
     def generate_pixels(self):
+        """
+        This function generates the pixels for the mandelbrot set. I use a combination of packages here. The meshgrid function is rather convenient for this use case since it creates a grid of coordinates. I then use the column_stack function to stack the x and y coordinates together.
+
+        I then use the starmap function from the multiprocessing package to calculate the escape time for each pixel. The starmap function is a bit like the map function, but it takes multiple arguments. The chunksize argument is used to specify how many arguments are sent to each process, this is done for computational purposes. 
+        """
         x_vals = np.linspace(0, self.img_width - 1, self.img_width)
         y_vals = np.linspace(0, self.img_height - 1, self.img_height)
         x_grid, y_grid = np.meshgrid(x_vals, y_vals)
@@ -42,6 +41,13 @@ class FractalRenderer:
             self.pixel_data = [self.calculate_escape_time(x, y) for x, y in coords]
 
     def calculate_escape_time(self, x, y):
+        """
+        Defining the escape time algorith to compute the values for the mandelbrot set. What is important here, is that we start an iteration from 1. The Mandelbrot set, is then the set of all the numbers that do not explode when fed into the equation. 
+
+        I also use the convert function which is defined after this function to convert the values in the mandelbrot set to the values on the canvas.
+
+        When the values have been converted to the canvas, it is possible to see, stable, unstable, semi-stable and the exploding values.
+        """
         real = convert(x, 0, self.img_width, self.x_min, self.x_max)
         imag = convert(y, 0, self.img_height, self.y_max, self.y_min)
         c = complex(real, imag)
@@ -53,6 +59,9 @@ class FractalRenderer:
         return (x, y, 0)
 
 def convert(value, min1, max1, min2, max2):
+    """
+    This function is used to convert the values in the mandelbrot set to the values on the canvas. The idea is to find the difference between the max and min values for the mandelbrot set and the canvas. Then we scale the values in the mandelbrot set to the values on the canvas. Otherwise the values would be too large to be displayed on the canvas.
+    """
     span1 = max1 - min1
     span2 = max2 - min2
     scaled_value = (value - min1) / span1
